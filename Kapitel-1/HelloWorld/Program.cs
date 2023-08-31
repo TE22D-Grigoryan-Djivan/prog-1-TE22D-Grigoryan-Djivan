c9bd07917591895ce4c8882497e9e85e0e7fc3c7
TE22D-Grigoryan-Djivan <djivan.grigoryan@elev.ga.ntig.se>
2023-08-31 10:17:32 +0200

    Tränat cw, cr färger

---
+++ Kapitel-1/HelloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.InteropServices;
+
+Console.Clear();
+Console.BackgroundColor = ConsoleColor.DarkMagenta;
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Hej, vad heter du?");
+string namn = Console.ReadLine();
+Console.BackgroundColor = ConsoleColor.DarkMagenta;
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Ett ord som beskriver dig?");
+string adjektiv = Console.ReadLine();
+Console.BackgroundColor = ConsoleColor.DarkMagenta;
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Din favorit mat?");
+string mat = Console.ReadLine();
+Console.BackgroundColor = ConsoleColor.DarkMagenta;
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Namn på någon du älskar:");
+string älskling = Console.ReadLine();
+
+
+Console.WriteLine("Hej! "+ namn +" du är " + adjektiv + " tycker " + älskling +". " + älskling +
+" har lagat din favorit maträtt " + mat + ", bon appetit!");
+
+Console.ReadLine();
---
+++ README.md
@@ -0,0 +1,35 @@
+[![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-718a45dd9cf7e7f842a935f5ebbe5719a5e09af4491e668f4dbf3b35d5cca122.svg)](https://classroom.github.com/online_ide?assignment_repo_id=11594443&assignment_repo_type=AssignmentRepo)
+# Mall
+Detta är en mall för repos för C#-programmering
+
+## Instruktioner
+
+1. Forka detta repo
+2. Skapa ett nytt C#-projekt i VS Code
+
+## Skapa C#-projekt
+
+1. Öppna VS Code
+2. Öppna en ny terminal
+
+```bash
+dotnet new console -o "namn på projektet"
+```
+
+3. Öppna projektet i VS Code
+
+```bash
+code "namn på projektet"
+```
+
+4. Koda och testa
+
+```bash
+dotnet run
+```
+
+## Ladda upp till GitHub
+
+1. Tryck på ikonen för Source Control
+2. Fyll i en commit meddelande som beskriver vad du har gjort
+3. Tryck på **Commit & Sync**
